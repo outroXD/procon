4b82000d35e62d1461f4d804740739bfc1f91d61
keisuke <keisuke.nakasuga@gmail.com>
2020-05-02 17:32:19 +0900

    ABC 162 ABC(外出先で作業するため途中でcommit)

---
+++ solvers/atcoder/ABC/ABC162/a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+using namespace std;
+
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i <= b; i++)
+#define all(a)  begin((a)),end((a))
+#define uniq(c) (c).erase(unique(all((c))), end((c)))
+
+int main()
+{
+    string N; cin >> N;
+    for (auto c : N) {
+        if (c == '7') {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+}
