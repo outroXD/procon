dbc1e4eb53ac33f66213822537e463a37afea92c
outroXD <keisuke.nakasuga+githubXD@gmail.com>
2020-06-17 02:53:22 +0900

    ABC 169 ABC

---
+++ solvers/atcoder/ABC/ABC169/b.py
@@ -0,0 +1,18 @@
+def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    if 0 in A:
+        print(0)
+        return
+
+    ans = 1
+    for a in A:
+        ans *= a
+        if ans > 1000000000000000000:
+            print(-1)
+            return
+
+    print(ans)
+
+main()
